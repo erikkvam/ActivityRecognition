61118aec1d89f5542df12919bba61584894ec5b0
erikkvam <erik.kvam@me.com>
2018-12-25 21:24:41 +0100

    Seccionat el codi font

--- test.R
+++ test.R
@@ -1,22 +1,17 @@
-
+#### Library declarations ####
 library(MASS)
 library(nnet)
 library(caret)
 library(e1071)
+
+#### Set seed ####
 set.seed (4567)
 
-setLocalDir <- function() {
-  actualDir <- dirname(rstudioapi::getSourceEditorContext()$path)
-  setwd(actualDir)
-}
-
-setLocalDir()
-
+#### Read data ####
 activity_labels <- read.table(file = "UCI_HAR_Dataset/activity_labels.txt", header=FALSE)
 features <- read.table(file = "UCI_HAR_Dataset/features.txt", header=FALSE)
 
-#0) Construcció dels datasets
-#Construcció del dataset de train
+#### Construcció del dataset de train ####
 x_train <- read.table(file = "UCI_HAR_Dataset/train/X_train.txt", header=FALSE)
 y_train <- read.table(file = "UCI_HAR_Dataset/train/y_train.txt", header=FALSE)
 subjects <- read.table(file = "UCI_HAR_Dataset/train/subject_train.txt", header=FALSE)
@@ -24,40 +19,40 @@ subjects <- read.table(file = "UCI_HAR_Dataset/train/subject_train.txt", header=
 colnames(x_train) <- features$V2 #donem noms a les columnes dels features
 raw.train <- x_train
 
-#Construcció del dataset de test
+#### Construcció del dataset de test ####
 x_test <- read.table(file = "UCI_HAR_Dataset/test/X_test.txt", header=FALSE)
 y_test <- read.table(file = "UCI_HAR_Dataset/test/y_test.txt", header=FALSE)
 subjects <- read.table(file = "UCI_HAR_Dataset/test/subject_test.txt", header=FALSE)
 
-colnames(x_test) <- features$V2 #definim quina 
+colnames(x_test) <- features$V2 #definim quina
 raw.test <- x_test
 
 
 
-#1) --Selecció de variables a utilitzar--
+#### 1) Selecció de variables a utilitzar ####
 
 #Busquem les components principals de les nostres dades de train
-pca.train <- prcomp(raw.train,center = T,retx = T) 
+pca.train <- prcomp(raw.train,center = T,retx = T)
 
 #predim les components principals de les nostres dades de test
 pca.test <- predict(pca.train, newdata = raw.test)
 
-#les potejem per veure com triar quants components principals 
-#per explicat la major part de les nostres dades 
+#Les plotejem per veure com triar quants components principals
+#per explicat la major part de les nostres dades
 plot(pca.train, type = "l")
 
-vars <- apply(pca.train$x, 2, var)  
+vars <- apply(pca.train$x, 2, var)
 props <- vars / sum(vars)
 acumul <- cumsum(props)
-plot(acumul[1:200], type = "l", xlab = "Nº Components Principals", ylab = "% Explicat", log = "x", panel.first = grid())
-plot(acumul[1:200], type = "l", xlab = "Nº Components Principals", ylab = "% Explicat", panel.first = grid())
+plot(acumul[1:200], type = "l", xlab = "N? Components Principals", ylab = "% Explicat", log = "x", panel.first = grid())
+plot(acumul[1:200], type = "l", xlab = "N? Components Principals", ylab = "% Explicat", panel.first = grid())
 #podem observar que:
 #amb 34 expliquem el 90% de les dades
 #amb 67 expliquem el 95% de les dades
 #amb 155 expliquem el 99% de les dades
 
 
-#2) --Perprocessem les dades-- 
+#### 2) Preprocessem les dades ####
 npcs <- 67
 data.train <- pca.train$x[,1:npcs]
 data.train <- as.data.frame(cbind(data.train, activity=y_train$V1)) #afegim la columa de l'activitat que esta realitzant
@@ -67,18 +62,13 @@ data.test <- pca.test[,1:npcs]
 data.test <- as.data.frame(cbind(data.test, activity=y_test$V1)) #afegim la columa de l'activitat que esta realitzant
 data.test[,npcs+1] <- factor(data.test$activity, labels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"))
 
-#3) --Metodes lineals--
-
-
-
-#3.3) SVM Lineal
+#### 3) Metodes lineals ####
+#### 3.3) SVM Lineal ####
 (C <- 2^(3:5))
 model.CV.SVMLIN <- tune(svm, activity ~., data=data.train, kernel="linear", ranges=list(cost=C))
 
-####################################
 save(model.CV.SVMLIN, file = "CV-SVMLIN.mod")
 load ("CV-SVMLIN.mod")
-####################################
 
 (model.CV.SVMLIN)
 
@@ -93,14 +83,12 @@ p2 <- as.factor(predict (model.svmLine, newdata=data.test, type="class"))
 (encerts <- sum(p2 == data.test$activity))
 (error_rate.valid <- 100*(1-(encerts)/nrow(data.test)))
 
-#3.4) SVM Quadratic
+#### 3.4) SVM Quadratic ####
 (C <- 2^(4:6))
 model.CV.SVMQUAD <- tune(svm, activity ~., data=data.train, kernel="polynomial", degree=2, coef0=1, ranges=list(cost=C))
 
-####################################
 save(model.CV.SVMQUAD, file = "CV-SVMQUAD.mod")
 load ("CV-SVMQUAD.mod")
-####################################
 
 (model.CV.SVMQUAD)
 
@@ -116,9 +104,9 @@ p2 <- as.factor(predict (model.svmQuad, newdata=data.test, type="class"))
 (error_rate.valid <- 100*(1-(encerts)/nrow(data.test)))
 
 
-#4) --Metodes no lieals--
-#4.1) MLP
-#Primer intentem buscar un nombre de neurones ocultes que sobreajustin. 
+#### 4) Mètodes no lineals ####
+#### 4.1) MLP ####
+#Primer intentem buscar un nombre de neurones ocultes que sobreajustin.
 HiddenUnits <- 12
 
 model.nnet <- nnet(activity ~., data = data.train, size=HiddenUnits, maxit=500, MaxNWts = 1200)
@@ -133,7 +121,7 @@ p2 <- as.factor(predict (model.nnet, newdata=data.test, type="class"))
 (error_rate.valid <- 100*(1-(encerts)/nrow(data.test)))
 
 
-#es pot observar que amb 12 neurones ocultes, el model esta sobreajustant, ja que el error de train es 0, 
+#es pot observar que amb 12 neurones ocultes, el model esta sobreajustant, ja que el error de train es 0,
 #i el error de test augmenta si el comparem amb models on hi ha menys neurones ocultes.
 
 #Ara, a traves de cross validation, buscarem quin es el millor valor de decay, per tal de regularitzar el model.
@@ -141,17 +129,17 @@ p2 <- as.factor(predict (model.nnet, newdata=data.test, type="class"))
 
 trc <- trainControl(method="repeatedcv", number=10, repeats=10)
 
-model.10x10CV <- train(activity ~., data=data.train, method='nnet', maxit = 500, trace = FALSE, 
+model.10x10CV <- train(activity ~., data=data.train, method='nnet', maxit = 500, trace = FALSE,
                         tuneGrid = expand.grid(.size=12,.decay=decays),trainControl = trc)
 
-####################################
+
 save(model.10x10CV, file = "10x10CV-MLP.mod")
 load ("10x10CV-MLP.mod")
-####################################
 
-#Mirem quin es el valor de regularitzacio que millor ajusta el nostre model 
+
+#Mirem quin es el valor de regularitzacio que millor ajusta el nostre model
 model.10x10CV$bestTune
-  
+
 #El model final de MLP es el seguent:
 model.nnet <- nnet(activity ~., data = data.train, size=HiddenUnits, maxit=700, decay = 0.4466836)
 
@@ -166,9 +154,9 @@ p2 <- as.factor(predict (model.nnet, newdata=data.test, type="class"))
 
 
 
-#4.2) SVM amb el kernel RBF
+#### 4.2) SVM amb el kernel RBF ####
 
-#Fem una primera aproximació sense tocar els hiper-parametres
+#Fem una primera aproximaci? sense tocar els hiper-parametres
 (model.svm <- svm(activity ~., data=data.train, type="C-classification", cost=1, kernel="radial", scale = FALSE))
 
 p1 <- as.factor(predict (model.svm, type="class"))
@@ -184,13 +172,13 @@ p2 <- as.factor(predict (model.svm, newdata=data.test, type="class"))
 #Un cop vist que els resultats son bons, pero poden ser millors si apliquem CV al model per trobar els hiper-parametres
 (model.CV.SVMRBF <- tune(svm, activity ~ ., data = data.train, ranges = list(gamma = G, cost = C),
             tunecontrol = tune.control(sampling = "fix")))
-####################################
+
 save(model.CV.SVMRBF, file = "CV-SVMRBF.mod")
 load ("CV-SVMRBF.mod")
-####################################
+
 
 model.CV.SVMRBF
-#Tal com es pot veure ens els resultat del CV, els millors valors per els hiper-parametres gamma i cost són,  0.015625 i 8 respectivament.
+#Tal com es pot veure ens els resultat del CV, els millors valors per els hiper-parametres gamma i cost s?n,  0.015625 i 8 respectivament.
 
 (model.svm <- svm(activity ~., data=data.train, type="C-classification", cost=8, gamma = 0.015625, kernel="radial", scale = FALSE))
 
